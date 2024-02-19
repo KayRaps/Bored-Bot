e93680810ca78b6a204be49fb4c658b2ba6da996
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-02-19 22:43:41 +0200

    added js

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ back from the Bored API
         excitement to the page
 */
 
+document.getElementById("get-activity").addEventListener("click", function(){
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("activity").textContent = data.activity
+        document.getElementById("title").textContent = " HappyBot "
+        document.querySelector("main").classList.add("fun")
+    })
+})
